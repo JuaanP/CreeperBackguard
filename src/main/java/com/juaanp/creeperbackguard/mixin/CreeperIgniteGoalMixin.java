3f896b5d228572705eaea0fa1fd400131eb84f42
JuaanP <Juaanp.br1@gmail.com>
2023-07-06 12:01:05 -0300

    1

--- src/main/java/com/juaanp/creeperbackguard/mixin/CreeperIgniteGoalMixin.java
+++ src/main/java/com/juaanp/creeperbackguard/mixin/CreeperIgniteGoalMixin.java
@@ -8,7 +8,6 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.CreeperIgniteGoal;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
