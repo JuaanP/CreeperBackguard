1c612983360a5cac141a096b282f838da5db86da
JuaanP <Juaanp.br1@gmail.com>
2023-07-06 13:58:39 -0300

    fix

--- src/main/java/com/juaanp/creeperbackguard/mixin/CreeperIgniteGoalMixin.java
+++ src/main/java/com/juaanp/creeperbackguard/mixin/CreeperIgniteGoalMixin.java
@@ -1,13 +1,12 @@
 package com.juaanp.creeperbackguard.mixin;
-
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.GameOptions;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.ai.goal.CreeperIgniteGoal;
-import net.minecraft.entity.mob.CreeperEntity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.Options;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.goal.SwellGoal;
+import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,42 +15,32 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(CreeperIgniteGoal.class)
+@Mixin(SwellGoal.class)
 public class CreeperIgniteGoalMixin {
-
     @Unique
-    private double fovScale;
-
-    @Shadow
-    private LivingEntity target;
-
-    @Shadow
-    @Final
-    private CreeperEntity creeper;
-
+    private double creeperBackguard$fovScale;
+    @Shadow private LivingEntity target;
+    @Shadow @Final private Creeper creeper;
     @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     private void creeperIgnitionGoal(CallbackInfo ci) {
-        if (this.target instanceof PlayerEntity) {
-
-            LivingEntity player = this.target;
-            LivingEntity creeper = this.creeper;
-
-            if (player.world.isClient){
-                MinecraftClient minecraftClient = MinecraftClient.getInstance();
-                if(minecraftClient != null){
-                    GameOptions gameOptions = minecraftClient.options;
+        if (this.target instanceof Player) {
+            Entity player = this.target;
+            Entity creeper = this.creeper;
+            if (player.level.isClientSide) {
+                Minecraft minecraft = Minecraft.getInstance();
+                if (minecraft != null) {
+                    Options gameOptions = minecraft.options;
                     if (gameOptions != null) {
-                        fovScale = Math.min(gameOptions.getFov().getValue(), 80);
+                        creeperBackguard$fovScale = Math.min(gameOptions.fov().get(), 80);
                     }
                 }
             } else {
-                fovScale = 70;
+                creeperBackguard$fovScale = 70;
             }
-
-            Vec3d targetPos = player.getCameraPosVec(1.0f);
-            Vec3d creeperPos = creeper.getCameraPosVec(1.0f);
-            Vec3d distance = creeperPos.subtract(targetPos);
-            if (!(player.getRotationVec(1.0f).dotProduct(distance.normalize()) >= Math.cos(Math.toRadians(fovScale)))) {
+            Vec3 targetPos = player.getEyePosition(1.0f);
+            Vec3 creeperPos = creeper.getEyePosition(1.0f);
+            Vec3 distance = creeperPos.subtract(targetPos);
+            if (!(player.getViewVector(1.0f).dot(distance.normalize()) >= Math.cos(Math.toRadians(creeperBackguard$fovScale)))) {
                 ci.cancel();
             }
         }
