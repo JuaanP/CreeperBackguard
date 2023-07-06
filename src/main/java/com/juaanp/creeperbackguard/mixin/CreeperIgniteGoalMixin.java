5fcd4bbdb1bd66cdac671b153df54205a17a6728
JuaanP <Juaanp.br1@gmail.com>
2023-07-06 10:58:28 -0300

    test

--- src/main/java/com/juaanp/creeperbackguard/mixin/CreeperIgniteGoalMixin.java
+++ src/main/java/com/juaanp/creeperbackguard/mixin/CreeperIgniteGoalMixin.java
@@ -1,7 +1,5 @@
 package com.juaanp.creeperbackguard.mixin;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.entity.LivingEntity;
