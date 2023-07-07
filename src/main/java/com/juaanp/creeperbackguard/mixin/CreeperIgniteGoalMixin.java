de0ba50ab0e6a649298264d2a449daeb65a7b265
JuaanP <Juaanp.br1@gmail.com>
2023-07-07 14:33:21 -0300

    Update CreeperIgniteGoalMixin.java

--- src/main/java/com/juaanp/creeperbackguard/mixin/CreeperIgniteGoalMixin.java
+++ src/main/java/com/juaanp/creeperbackguard/mixin/CreeperIgniteGoalMixin.java
@@ -7,6 +7,9 @@ import net.minecraft.entity.ai.goal.CreeperIgniteGoal;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Vec3d;
+
+import java.util.logging.ConsoleHandler;
+
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -40,6 +43,7 @@ public class CreeperIgniteGoalMixin {
                     GameOptions gameOptions = minecraftClient.options;
                     if (gameOptions != null) {
                         fovScale = Math.min(gameOptions.fov, 80);
+                        System.out.println(gameOptions.fov);
                     }
                 }
             } else {
