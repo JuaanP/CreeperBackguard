4f3452d03001e415c8d5d06ae8a89e546c872057
JuaanP <Juaanp.br1@gmail.com>
2023-07-09 13:36:30 -0300

    clean up

--- src/main/java/com/juaanp/creeperbackguard/mixins/SwellGoalMixinClient.java
+++ src/main/java/com/juaanp/creeperbackguard/mixins/SwellGoalMixinClient.java
@@ -8,6 +8,7 @@ import net.minecraft.world.entity.ai.goal.SwellGoal;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -30,7 +31,7 @@ public class SwellGoalMixinClient {
         if (this.target instanceof Player) {
 
             Player player = (Player) this.target;
-            LivingEntity creeper = this.creeper;
+            Creeper creeper = this.creeper;
 
             Minecraft minecraft = Minecraft.getInstance();
 
@@ -40,10 +41,11 @@ public class SwellGoalMixinClient {
                 fovScale = 70;
             }
 
-            Vec2 targetPos = new Vec2((float) player.getX(), (float) player.getZ());
-            Vec2 creeperPos = new Vec2((float) creeper.getX(),(float) creeper.getZ());
-            Vec2 distance = new Vec2(creeperPos.x - targetPos.x, creeperPos.y - targetPos.y);
-            if (!(Mth.cos(player.getYRot() * 0.017453292F) * distance.x + Mth.sin(player.getYRot() * 0.017453292F) * distance.y >= Math.cos(Math.toRadians(fovScale)))) {
+            Vec3 playerPos = player.getEyePosition();
+            Vec3 creeperPos = creeper.getEyePosition();
+            Vec3 distance = creeperPos.subtract(playerPos);
+
+            if (!(player.getLookAngle().dot(distance.normalize()) >= Math.cos(Math.toRadians(fovScale)))) {
                 ci.cancel();
             }
         }
