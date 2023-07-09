31157708d430039de56c08ea39eaa22df8d07e31
JuaanP <Juaanp.br1@gmail.com>
2023-07-09 14:44:34 -0300

    clean up

--- src/main/java/com/juaanp/creeperbackguard/CreeperBackguard.java
+++ src/main/java/com/juaanp/creeperbackguard/CreeperBackguard.java
@@ -1,21 +1,10 @@
-package com.example.examplemod;
+package com.juaanp.creeperbackguard;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-import java.util.stream.Collectors;
 
 @Mod("creeperbackguard")
 public class CreeperBackguard
